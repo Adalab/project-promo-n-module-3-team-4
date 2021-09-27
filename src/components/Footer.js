437b20e2c85161d724e0f7651d3754c8e88089ab
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-27 20:15:32 +0200

    tried components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 //import "../styles/core/variables.scss";
-import "../styles/core/reset.scss";
-import "../styles/core/mixins.scss";
-import "../styles/layout/footer.scss";
-import logoAdalab from "../images/logo-adalab.png";
+import '../styles/core/reset.scss';
+import '../styles/core/mixins.scss';
+import '../styles/layout/footer.scss';
+import logoAdalab from '../images/logo-adalab.png';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <small className="info-footer">awesom profile-cards @2018</small>
-      <img className="logo-adalab" src={logoAdalab} alt="logo-adalab" />
+    <footer className='footer'>
+      <small className='info-footer'>awesom profile-cards @2018</small>
+      <img className='logo-adalab' src={logoAdalab} alt='logo-adalab' />
     </footer>
   );
 };
