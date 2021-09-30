1aaba062e16ee4d3439898add37c91b75e4b51ac
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-30 16:39:10 +0200

    add api code from dayana

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,19 +1,29 @@
-// import { useEffect } from 'react';
+const callToApi = (data) => {
+  return fetch('https://awesome-profile-cards.herokuapp.com/card', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
 
-// const callToApi = (setData) => {
-//   return fetch(' https://awesome-profile-cards.herokuapp.com/card')
-//     .then((response) => response.json())
-//     .then((response) => {
-//       setData(response);
-//     });
-// };
+export default callToApi;
 
-// export default callToApi;
-
-// fetch('https://awesome-profile-cards.herokuapp.com/card', {
-//     method: 'POST',
-//     body: JSON.stringify(data),
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   })
+// const postDataFetch = (dataState) => {
+//     return fetch(URL, {
+//       method: 'POST',
+//       body: JSON.stringify(dataState),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         return data;
+//       });
+//   };
