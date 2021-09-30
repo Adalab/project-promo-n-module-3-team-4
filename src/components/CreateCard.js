05e74c7fe1b4e9312d39f3834bd95d9bd0674ce6
Soraya Alonso <salonsocuesta@gmail.com>
2021-09-30 19:40:59 +0200

    Fetch doesnt run

--- src/components/CreateCard.js
+++ src/components/CreateCard.js
@@ -5,7 +5,7 @@ const CreateCard = (props) => {
         <i className="far fa-address-card create__address"></i>crear tarjeta
       </button>
 
-      <div className="created hidden js-cardCreated">
+      <div className="created hidden">
         <h3 className="created__title">La tarjeta ha sido creada:</h3>
         {/* <a rel="noreferrer" href="" target="_blank" className="created__url js-url"></a> */}
         <button className="created__btn twitter--button">
@@ -24,3 +24,71 @@ const CreateCard = (props) => {
 };
 
 export default CreateCard;
+
+////////en proceso, no borrar para back ///////////////
+
+// import { useState } from "react";
+
+
+// function Share(props) {
+// //  const [pepino, setPepino] = useState('');
+ 
+//   const handleCreateCard = (data) => {
+
+//     fetch('https://awesome-profile-cards.herokuapp.com/card', {
+//           method: 'POST',
+//           body: JSON.stringify(data),
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//         })
+//           .then((response) => response.json())
+//           .then((data) => {
+             
+//             console.log(data);
+            
+         
+
+//         // const resultData = data.pepino;
+//         // if (pepino === '') {
+          
+//         // } else {
+//         //   setStatus('Me ha dado ok');
+//         //   setURL(result);
+//         //   console.log(result);
+//         // }
+//       });
+//     };
+  
+
+//   return (
+//     <section className={`create ${props.collapsable3}`}>
+//       <button className="create__btn js-createBtn" name="create">
+//         <i className="far fa-address-card create__address" onClick={handleCreateCard}></i>crear tarjeta
+//       </button>
+
+//       <div className="created ">
+//         <h3 className="created__title">La tarjeta ha sido creada:</h3>
+//         {/* <a rel="noreferrer" href="" target="_blank" className="created__url js-url"></a> */}
+//         <button className="created__btn twitter--button">
+//           <a
+//             rel="noreferrer"
+//             href="/#"
+//             className="created__btn--text twitter--link"
+//           >
+//             <i className="fab fa-twitter created__btn--icon"></i>
+//             Compartir en Twitter
+//           </a>
+//         </button>
+//       </div>
+//     </section>
+//   );
+
+//    }
+
+
+
+// export default Share;
+
+
+
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,51 +1,3 @@
-// import { useState } from 'react';
-// import ErrorMessage from './ErrorMessage';
-// import SuccessMessage from './SuccessMessage';
-
-// function Share(props) {
-//   const [status, setStatus] = useState('No enviado'); // "No enviado", "Me ha dado error", "Me ha dado ok"
-//   const [cardURL, setURL] = useState('');
-
-//   const handleCreateCard = () => {
-//     const routeId = 'card';
-//     fetch('/card', {
-//       method: 'POST',
-//       headers: {
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(props.data),
-//     })
-//       .then((response) => response.json())
-//       .then((res) => {
-//         console.log(res);
-//         const result = res.cardURL;
-//         if (res.success === false) {
-//           setStatus('Me ha dado error');
-//         } else {
-//           setStatus('Me ha dado ok');
-//           setURL(result);
-//           console.log(result);
-//         }
-//       });
-//   };
-
-//   return (
-//     <div className='share-container'>
-//       <div className='sharebutton' onClick={handleCreateCard}>
-//         <i className='far fa-address-card list-icon2c js-hidden'></i>
-//         <div className='new-card js-create-card'>Create Card</div>
-//       </div>
-
-//       {status === 'Me ha dado error' ? <ErrorMessage /> : null}
-//       {status === 'Me ha dado ok' ? <SuccessMessage url={cardURL} /> : null}
-//     </div>
-//   );
-// }
-
-// export default Share;
-
-
 const callToApi = (data) => {
   return fetch('https://awesome-profile-cards.herokuapp.com/card', {
     method: 'POST',
@@ -56,6 +8,7 @@ const callToApi = (data) => {
   })
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       return data;
       
     });
