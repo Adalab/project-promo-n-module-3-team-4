3094a906c26625fb97a3c0262b3b4946b62f15d0
Soraya Alonso <salonsocuesta@gmail.com>
2021-10-11 13:33:16 +0200

    Added new changes

--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,9 +1,9 @@
 const callToApi = (data) => {
-  return fetch("https://awesome-profile-cards.herokuapp.com/card", {
-    method: "POST",
+  return fetch('//localhost:3001/card', {
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   }).then((response) => response.json());
 };
