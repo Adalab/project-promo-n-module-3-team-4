ebd075d319ec8a5c34609c378ce78b2903c594fb
Soraya Alonso <salonsocuesta@gmail.com>
2021-10-14 14:02:03 +0200

    Added conditional

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require("express");
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
 // Creamos el servidor
 const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({ limit: "10mb" }));
+server.use(express.json({ limit: '10mb' }));
 
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 3001;
@@ -17,37 +17,63 @@ server.listen(serverPort, () => {
 });
 
 // Escribimos los endpoints que queramos
-server.get("/card/:id", (req, res) => {
-  console.log("Se mostraría la tarjeta");
-  res.send("Se mostraría la tarjeta.");
+server.get('/card/:id', (req, res) => {
+  console.log('Se mostraría la tarjeta');
+  res.send('Se mostraría la tarjeta.');
 });
 
-server.post("/card", (req, res) => {
-  console.log("body:", req.body);
+server.post('/card', (req, res) => {
+  console.log('body:', req.body);
 
-  if (true) {
+  if (req.body.name === '') {
+    const response = {
+      success: false,
+      error: 'Falta el campo del nombre.',
+    };
+    res.json(response);
+  } else if (req.body.job === '') {
+    const response = {
+      success: false,
+      error: 'Falta el campo del puesto.',
+    };
+    res.json(response);
+  } else if (req.body.email === '') {
+    const response = {
+      success: false,
+      error: 'Falta el campo del email.',
+    };
+    res.json(response);
+  } else if (req.body.linkedin === '') {
+    const response = {
+      success: false,
+      error: 'Falta el campo del Linkedin.',
+    };
+    res.json(response);
+  } else if (req.body.photo === '') {
+    const response = {
+      success: false,
+      error: 'Falta el campo de la foto.',
+    };
+    res.json(response);
+  } else {
     const identificadorUnico = uuidv4();
     const response = {
       success: true,
       cardURL: `http://localhost:${serverPort}/card/${identificadorUnico}`,
     };
     res.json(response);
-  } else {
-    const response = {
-      success: false,
-      error: "Todavía no he implementado esta movida.",
-    };
-    res.json(response);
   }
 });
 
-const staticServerPathWeb = "./public"; // En esta carpeta ponemos los ficheros estáticos
+server.get('/card/:id', (req, res) => {});
+
+const staticServerPathWeb = './public'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
 // Endpoint para gestionar los errores 404
-server.get("*", (req, res) => {
+server.get('*', (req, res) => {
   // Relativo a este directorio
-  const notFoundFileRelativePath = "../public/404-not-found.html";
+  const notFoundFileRelativePath = '../public/404-not-found.html';
   const notFoundFileAbsolutePath = path.join(
     __dirname,
     notFoundFileRelativePath
--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,25 +1,25 @@
-/*const bodyParams = {
-  userName: "Maricarmen",
-  userEmail: "mari@gmail.com",
-};
-fetch("http://localhost:3000/user", {
-  method: "POST",
-  body: JSON.stringify(bodyParams),
-  headers: {
-    "Content-Type": "application/json",
-  },
-});*/
+// const bodyParams = {
+//   userName: "Maricarmen",
+//   userEmail: "mari@gmail.com",
+// };
+// fetch("http://localhost:3000/user", {
+//   method: "POST",
+//   body: JSON.stringify(bodyParams),
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
 const callToApi = (data) => {
-  return fetch("//localhost:3001/card", {
-    method: "POST",
+  return fetch('//localhost:3001/card', {
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   })
     .then((response) => response.json())
-    .catch((error) => console.log("Error:", error));
+    .catch((error) => console.log('Error:', error));
 };
 
 export default callToApi;
