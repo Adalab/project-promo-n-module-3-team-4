6b99e367dfd2cb3c7996bc2524b2d4ce95f8f1f8
Soraya Alonso <salonsocuesta@gmail.com>
2021-10-18 14:02:47 +0200

    Added changes, not run

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require("express");
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 // importar el módulo better-sqlite3
-const Database = require("better-sqlite3");
+const Database = require('better-sqlite3');
 
 // Creamos el servidor
 const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({ limit: "10mb" }));
-server.set("view engine", "ejs");
+server.use(express.json({ limit: '10mb' }));
+server.set('view engine', 'ejs');
 
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 3001;
@@ -20,75 +20,88 @@ server.listen(serverPort, () => {
 });
 
 // indicar qué base de datos vamos a usar con la ruta relativa a la raíz del proyecto
-const db = new Database("./src/db/dataBase.db", {
+const db = new Database('./src/db/dataBase.db', {
   // con verbose le decimos que todas las queries que se ejecuten las muestre en la consola
   verbose: console.log,
   // así podemos comprobar qué queries estamos haciendo en todo momento
 });
 
 // Escribimos los endpoints que queramos
-server.get("/card/:id", (req, res) => {
-  console.log("Se mostraría la tarjeta");
-  res.send("Se mostraría la tarjeta.");
+server.get('/card/:id', (req, res) => {
+  console.log(req.params);
+
+  const query = db.prepare('SELECT * FROM cards WHERE uuid=?');
+  const cardData = query.get(req.params.id);
+
+  console.log(cardData);
+
+  res.render('preview', cardData);
 });
 
-server.post("/card", (req, res) => {
-  console.log("body:", req.body);
+server.post('/card', (req, res) => {
+  console.log('body:', req.body);
 
-  if (req.body.name === "") {
+  if (req.body.name === '') {
     const response = {
       success: false,
-      error: "Falta el campo del nombre.",
+      error: 'Falta el campo del nombre.',
     };
     res.json(response);
-  } else if (req.body.job === "") {
+  } else if (req.body.job === '') {
     const response = {
       success: false,
-      error: "Falta el campo del puesto.",
+      error: 'Falta el campo del puesto.',
     };
     res.json(response);
-  } else if (req.body.email === "") {
+  } else if (req.body.email === '') {
     const response = {
       success: false,
-      error: "Falta el campo del email.",
+      error: 'Falta el campo del email.',
     };
     res.json(response);
-  } else if (req.body.linkedin === "") {
+  } else if (req.body.linkedin === '') {
     const response = {
       success: false,
-      error: "Falta el campo del Linkedin.",
+      error: 'Falta el campo del Linkedin.',
     };
     res.json(response);
-  } else if (req.body.photo === "") {
+  } else if (req.body.photo === '') {
     const response = {
       success: false,
-      error: "Falta el campo de la foto.",
+      error: 'Falta el campo de la foto.',
     };
     res.json(response);
   } else {
     const identificadorUnico = uuidv4();
+    // Aquí ponme un INSERT Soraya.
+    const query = db.prepare(
+      'INSERT INTO cards (palette, name, job, email, phone, linkedin, github, photo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+    );
+    const result = query.run(
+      req.body.palette,
+      req.body.name,
+      req.body.job,
+      req.body.email,
+      req.body.phone,
+      req.body.linkedin,
+      req.body.github,
+      req.body.photo
+    );
     const response = {
       success: true,
-      cardURL: `http://localhost:${serverPort}/card/${identificadorUnico}`,
+      cardURL: `http://localhost:${serverPort}/card/${result.identificadorUnico}`,
     };
     res.json(response);
   }
 });
 
-server.get("/card/:id", (req, res) => {
-  // preparamos y ejecutamos la query
-  const query = db.prepare("SELECT * FROM cards");
-  const cards = query.get(); // respondemos a la petición con los datos que ha devuelto la base de datos
-  res.json({ cards: cards });
-});
-
-const staticServerPathWeb = "./public"; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathWeb = './public'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
 // Endpoint para gestionar los errores 404
-server.get("*", (req, res) => {
+server.get('*', (req, res) => {
   // Relativo a este directorio
-  const notFoundFileRelativePath = "../public/404-not-found.html";
+  const notFoundFileRelativePath = '../public/404-not-found.html';
   const notFoundFileAbsolutePath = path.join(
     __dirname,
     notFoundFileRelativePath
