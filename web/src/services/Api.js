468af5602ad22c41e2280996fc5d5b6c7fb82657
Soraya Alonso <salonsocuesta@gmail.com>
2021-10-20 13:24:40 +0200

    added

--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,13 +1,13 @@
 const callToApi = (data) => {
-  return fetch("/card", {
-    method: "POST",
+  return fetch('/card', {
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   })
     .then((response) => response.json())
-    .catch((error) => console.log("Error:", error));
+    .catch((error) => console.log('Error:', error));
 };
 
 export default callToApi;
