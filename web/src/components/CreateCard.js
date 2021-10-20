5fe26e52765828ca51f6e9deac4ec68c9b142bb2
MiriamVF <miriam.verduf@gmail.com>
2021-10-20 12:31:31 +0200

    fix fetch

--- web/src/components/CreateCard.js
+++ web/src/components/CreateCard.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Api from '../services/Api';
+import Api from "../services/Api";
 
 const CreateCard = (props) => {
   //fetch states
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
-  const [cardURL, setCardURL] = useState('');
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [cardURL, setCardURL] = useState("");
 
   const handleFetch = (ev) => {
     ev.preventDefault();
@@ -15,117 +15,58 @@ const CreateCard = (props) => {
       if (responseData.success === false) {
         setError(responseData.error);
 
-        setSuccess('');
+        setSuccess("");
       } else if (responseData.success === true) {
         setSuccess(responseData.cardURL);
-        setError('');
+        setError("");
       }
     });
   };
 
   return (
     <section className={`create ${props.collapsable3}`}>
-      <button className='create__btn' name='create' onClick={handleFetch}>
-        <i className='far fa-address-card create__address'></i>crear tarjeta
+      <button className="create__btn" name="create" onClick={handleFetch}>
+        <i className="far fa-address-card create__address"></i>crear tarjeta
       </button>
 
-      {success !== '' ? (
-        <div className='created'>
-          <h3 className='created__title'>La tarjeta ha sido creada:</h3>
+      {success !== "" ? (
+        <div className="created">
+          <h3 className="created__title">La tarjeta ha sido creada:</h3>
           <a
-            title='Link'
-            rel='noreferrer'
+            title="Link"
+            rel="noreferrer"
             href={cardURL}
-            target='_blank'
-            className='created__url'
+            target="_blank"
+            className="created__url"
           >
             {cardURL}
           </a>
-          <button className='created__btn twitter--button'>
+          <button className="created__btn twitter--button">
             <a
-              rel='noreferrer'
+              rel="noreferrer"
               href={success}
-              target='_blank'
-              className='created__btn--text twitter--link'
+              target="_blank"
+              className="created__btn--text twitter--link"
             >
-              <i className='fab fa-twitter created__btn--icon'></i>
+              <i className="fab fa-twitter created__btn--icon"></i>
               Compartir en Twitter
             </a>
           </button>
         </div>
       ) : (
-        ''
+        ""
       )}
 
-      {error !== '' ? (
-        <div className='created'>
-          <h3 className='created__title'>La tarjeta NO ha sido creada:</h3>
+      {error !== "" ? (
+        <div className="created">
+          <h3 className="created__title">La tarjeta NO ha sido creada:</h3>
           <p>{error}</p>
         </div>
       ) : (
-        ''
+        ""
       )}
     </section>
   );
 };
 
 export default CreateCard;
-
-////////en proceso, no borrar para back ///////////////
-
-// import { useState } from "react";
-
-// function Share(props) {
-// //  const [pepino, setPepino] = useState('');
-
-//   const handleCreateCard = (data) => {
-
-//     fetch('https://awesome-profile-cards.herokuapp.com/card', {
-//           method: 'POST',
-//           body: JSON.stringify(data),
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//         })
-//           .then((response) => response.json())
-//           .then((data) => {
-
-//             console.log(data);
-
-//         // const resultData = data.pepino;
-//         // if (pepino === '') {
-
-//         // } else {
-//         //   setStatus('Me ha dado ok');
-//         //   setURL(result);
-//         //   console.log(result);
-//         // }
-//       });
-//     };
-
-//   return (
-//     <section className={`create ${props.collapsable3}`}>
-//       <button className="create__btn js-createBtn" name="create">
-//         <i className="far fa-address-card create__address" onClick={handleCreateCard}></i>crear tarjeta
-//       </button>
-
-//       <div className="created ">
-//         <h3 className="created__title">La tarjeta ha sido creada:</h3>
-//         {/* <a rel="noreferrer" href="" target="_blank" className="created__url js-url"></a> */}
-//         <button className="created__btn twitter--button">
-//           <a
-//             rel="noreferrer"
-//             href="/#"
-//             className="created__btn--text twitter--link"
-//           >
-//             <i className="fab fa-twitter created__btn--icon"></i>
-//             Compartir en Twitter
-//           </a>
-//         </button>
-//       </div>
-//     </section>
-//   );
-
-//    }
-
-// export default Share;
--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,39 +1,13 @@
-// const bodyParams = {
-//   userName: "Maricarmen",
-//   userEmail: "mari@gmail.com",
-// };
-// fetch("http://localhost:3000/user", {
-//   method: "POST",
-//   body: JSON.stringify(bodyParams),
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
 const callToApi = (data) => {
-  return fetch('//localhost:3001/card', {
-    method: 'POST',
+  return fetch("/card", {
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   })
     .then((response) => response.json())
-    .catch((error) => console.log('Error:', error));
+    .catch((error) => console.log("Error:", error));
 };
 
 export default callToApi;
-
-// const postDataFetch = (dataState) => {
-//     return fetch(URL, {
-//       method: 'POST',
-//       body: JSON.stringify(dataState),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         return data;
-//       });
-//   };
